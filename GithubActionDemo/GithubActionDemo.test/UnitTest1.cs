b96c48f11e4fa9fa123699415855c2f1a9759eb5
q12389 <q12389@yahoo.com.tw>
2022-12-18 16:11:21 +0800

    initial

---
+++ GithubActionDemo/GithubActionDemo.test/UnitTest1.cs
@@ -0,0 +1,25 @@
+namespace GithubActionDemo.test
+{
+    public class Tests
+    {
+   
+        [SetUp]
+        public void Setup()
+        {
+     
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        [TestCase(1, 2, 3)]
+        [TestCase(1, 2, 3)]
+        public void Add(int a, int b, int sum)
+        {
+            Assert.That(a+ b, Is.EqualTo(sum));
+        }
+    }
+}
